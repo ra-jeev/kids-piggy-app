5dc3c524d084a9f632e34bc63cc2e8788a885863
Rajeev R Sharma <i.rarsh@gmail.com>
2022-10-19 23:02:41 +0530

    Adding child profie pic feature with cropping

--- src/components/ChildCard.js
+++ src/components/ChildCard.js
@@ -1,6 +1,10 @@
+import { useEffect, useRef, useState } from 'react';
+
+import { Storage } from '@aws-amplify/storage';
 import {
   Flex,
   Heading,
+  Image,
   Card,
   View,
   Text,
@@ -8,20 +12,100 @@ import {
   Button,
   Alert,
 } from '@aws-amplify/ui-react';
-import { MdAccountCircle, MdAddCircle, MdRemoveCircle } from 'react-icons/md';
+import { MdAddCircle, MdRemoveCircle, MdPhotoCamera } from 'react-icons/md';
+import { FaUserCircle } from 'react-icons/fa';
 
 import { formatCurrency, formatDate } from '../utils';
+import { AddPhotoDialog } from './AddPhotoDialog';
 
 export const ChildCard = ({ child, currency, onTransact }) => {
+  const inputRef = useRef(null);
+  const [signedURL, setSignedURL] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedFileDataURL, setSelectedFileDataURL] = useState(null);
+
+  const onFilePicked = async (event) => {
+    if (event.target.files && event.target.files.length) {
+      const reader = new FileReader();
+      reader.addEventListener('load', () => {
+        const result = reader.result || null;
+        setSelectedFileDataURL(result);
+        setIsOpen(!!result);
+      });
+
+      reader.readAsDataURL(event.target.files[0]);
+    } else {
+      setSelectedFileDataURL(null);
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    const getPhotoUrl = async () => {
+      const url = await Storage.get(child.photoUrlKey, {
+        level: 'private',
+        download: false,
+      });
+
+      setSignedURL(url);
+    };
+
+    if (child && child.photoUrlKey) {
+      getPhotoUrl();
+    }
+  }, [child]);
+
   return (
     <Card borderRadius='medium' variation='elevated'>
       <Flex direction='column'>
         <Flex alignItems='center'>
-          <Icon
-            ariaLabel='person icon'
-            as={MdAccountCircle}
+          <View
+            position='relative'
             fontSize={{ base: '4rem', large: '5rem' }}
-          />
+            lineHeight='1'
+            height={{ base: '4rem', large: '5rem' }}
+          >
+            {signedURL ? (
+              <Image
+                alt={`${child.name}'s profile photo`}
+                src={signedURL}
+                height={{ base: '4rem', large: '5rem' }}
+                width={{ base: '4rem', large: '5rem' }}
+                objectFit='cover'
+                objectPosition='50% 50%'
+                borderRadius='50%'
+              />
+            ) : (
+              // <View fontSize={{ base: '4rem', large: '5rem' }} lineHeight='1'>
+              <FaUserCircle
+
+              // size={{ base: '4rem', large: '5rem' }}
+              />
+              // </View>
+            )}
+
+            <input
+              type='file'
+              style={{ display: 'none' }}
+              ref={inputRef}
+              accept='image/*'
+              onChange={onFilePicked}
+            />
+
+            <Button
+              position='absolute'
+              right='0'
+              bottom='0'
+              size='small'
+              padding='0.25rem'
+              borderRadius='xl'
+              backgroundColor='rgba(0, 0, 0, 0.48)'
+              onClick={() => inputRef.current.click()}
+            >
+              <Icon ariaLabel='Change photo' as={MdPhotoCamera} />
+            </Button>
+          </View>
+
           <Flex direction='column' gap='0.25rem'>
             <Heading level={4} fontSize={{ base: 'large', large: 'xl' }}>
               {child.name}
@@ -96,6 +180,15 @@ export const ChildCard = ({ child, currency, onTransact }) => {
           </Button>
         </Flex>
       </Flex>
+      <AddPhotoDialog
+        isOpen={isOpen}
+        child={child}
+        dataURL={selectedFileDataURL}
+        onClose={() => {
+          inputRef.current.value = '';
+          setIsOpen(false);
+        }}
+      />
     </Card>
   );
 };
